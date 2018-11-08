6fcad9e4b5703fcce9ab123c748fff82fbebe2b6
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-11-08 11:14:04 +0200

    Added optional tests

--- src/main/java/rptest/TestCases.java
+++ src/main/java/rptest/TestCases.java
@@ -19,6 +19,8 @@ package rptest;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.collections.ListUtils;
+
 public final class TestCases {
 
   public static final List<String> C_MANDATORY = Arrays.asList("rp-response_type-code",
@@ -120,5 +122,59 @@ public final class TestCases {
       "rp-id_token-issuer-mismatch",
       "rp-userinfo-bad-sub-claim",
       "rp-userinfo-bearer-header");
+  
+  public static final List<String> OPTIONAL_FOR_ALL = Arrays.asList("rp-discovery-webfinger-acct",
+      "rp-discovery-webfinger-http-href",
+      "rp-discovery-webfinger-url",
+      "rp-discovery-openid-configuration",
+      "rp-discovery-jwks_uri-keys",
+      "rp-discovery-issuer-not-matching-config",
+      "rp-discovery-webfinger-unknown-member",
+      "rp-registration-dynamic",
+      "rp-response_mode-form_post-error",
+      "rp-response_mode-form_post",
+      "rp-request_uri-enc",
+      "rp-request_uri-sig",
+      "rp-request_uri-sig+enc",
+      "rp-request_uri-unsigned",
+      "rp-id_token-sig+enc",
+      "rp-id_token-sig-hs256",
+      "rp-id_token-sig-es256",
+      "rp-id_token-sig+enc-a128kw",
+      "rp-id_token-bad-sig-hs256",
+      "rp-id_token-bad-sig-es256",
+      "rp-key-rotation-op-sign-key-native",
+      "rp-key-rotation-op-sign-key",
+      "rp-key-rotation-op-enc-key",
+      "rp-3rd_party-init-login");
+  
+  @SuppressWarnings("unchecked")
+  public static final List<String> C_OPTIONAL = ListUtils.union(OPTIONAL_FOR_ALL, Arrays.asList(
+      "rp-token_endpoint-private_key_jwt",
+      "rp-token_endpoint-client_secret_post",
+      "rp-token_endpoint-client_secret_jwt",
+      "rp-claims-distributed",
+      "rp-claims-aggregated",
+      "rp-userinfo-sig",
+      "rp-userinfo-bearer-body",
+      "rp-userinfo-enc",
+      "rp-userinfo-sig+enc"));
 
+  public static final List<String> CI_OPTIONAL = C_OPTIONAL;
+
+  public static final List<String> CIT_OPTIONAL = C_OPTIONAL;
+
+  @SuppressWarnings("unchecked")
+  public static final List<String> CT_OPTIONAL = ListUtils.union(OPTIONAL_FOR_ALL, Arrays.asList(
+      "rp-claims-distributed",
+      "rp-claims-aggregated",
+      "rp-userinfo-sig",
+      "rp-userinfo-bearer-body",
+      "rp-userinfo-enc",
+      "rp-userinfo-sig+enc"));
+
+  public static final List<String> I_OPTIONAL = OPTIONAL_FOR_ALL;
+
+  public static final List<String> IT_OPTIONAL = CT_OPTIONAL;
+  
 }
